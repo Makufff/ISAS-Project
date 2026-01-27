c3bbc69499ebf256a6a4e82221bfa44e0b3c56cd
Tanapat Chamted <91345773+Makufff@users.noreply.github.com>
2026-01-28 02:11:06 +0700

    Create test-docker-run.sh

---
+++ judgehost/test-docker-run.sh
@@ -0,0 +1,13 @@
+docker run -d \
+  --name judgehost \
+  --hostname judgehost \
+  --privileged \
+  --cgroupns=host \
+  --restart always \
+  -e DAEMON_ID=4 \
+  -e JUDGEDAEMON_PASSWORD=domjudgeisas \
+  -e DOMSERVER_BASEURL=http://192.168.17.108 \
+  -e CONTAINER_TIMEZONE=Asia/Bangkok \
+  -v /sys/fs/cgroup:/sys/fs/cgroup:rw \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  domjudge/judgehost:9.0.0
